e7c2b1486559c0cae8308812136c097c0648ca18
Iqbal1606 <72688860+Iqbal1606@users.noreply.github.com>
2022-03-31 09:01:12 +0700

    Add files via upload

---
+++ pratikum 4/no 1.py
@@ -0,0 +1,46 @@
+#nama   : iqbal hario syahputra
+#NIM    : 20051397029
+#KELAS  : D4 MI 2020A
+
+class Rectangle:
+    def __init__(self, width):
+        self.width = width
+
+    def area(self):
+        return pow(self.width, 2)
+
+    def around(self):
+        return 4 * self.width
+
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return (self.base * self.height) / 2
+
+    def around(self):
+        return self.base + self.height + (self.base * self.height)
+
+class Parrallelogram:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height
+
+    def around(self):
+        return self.base + self.height + (self.base * self.height)
+
+rectangle = Rectangle(10)
+triangle = Triangle(10, 5)
+parrallelogram = Parrallelogram(10, 5)
+
+print('Keliling Persegi = ',rectangle.around())
+print('Luas Persegi = ',rectangle.area())
+print('Keliling Segitiga = ',triangle.around())
+print('Luas Segitiga = ',triangle.area())
+print('Keliling Jajar Genjang = ',parrallelogram.around())
+print('Luas Jajar Genjang = ',parrallelogram.area())
